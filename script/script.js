c43adc9b367dec48084a8605c6fc77d0f938b3ea
Orsouene <orsoueneelaouizeb@gmail.com>
2024-10-29 13:54:54 +0100

    multipli di 5 e 3 , fine esercizio

--- script/script.js
+++ script/script.js
@@ -1,11 +1,15 @@
 for (i = 1; i <= 100; i++) {
-  //    multipli di 3
-  if (i % 3 === 0) {
-    console.log("Fizz");
+  //    multipli di 5 e 3
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("FizzBuzz");
   }
   //    multipli di 5
   else if (i % 5 === 0) {
     console.log("Buzz");
+  }
+  //    multipli di 3
+  else if (i % 3 === 0) {
+    console.log("Fizz");
   } else {
     console.log(i);
   }
