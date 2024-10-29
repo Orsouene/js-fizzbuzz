c467e2d523638355f1817e02819ee61e6cdc5b4c
Orsouene <orsoueneelaouizeb@gmail.com>
2024-10-29 13:42:43 +0100

    multipli di 5

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 for (i = 1; i <= 100; i++) {
+  //    multipli di 3
   if (i % 3 === 0) {
     console.log("Fizz");
+  }
+  //    multipli di 5
+  else if (i % 5 === 0) {
+    console.log("Buzz");
   } else {
     console.log(i);
   }
