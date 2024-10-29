9c9f60c8f695aec272ca88047986ddd31dfd23b6
Orsouene <orsoueneelaouizeb@gmail.com>
2024-10-29 14:02:37 +0100

    Aggiunto un commento

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// È un programma che stampa i numeri da 1 a 100. Per i multipli di 3 viene scritto 'Fizz', per i multipli di 5 viene scritto 'Buzz' e per i numeri che sono sia multipli di 3 che di 5 viene scritto 'FizzBuzz'
+
 for (i = 1; i <= 100; i++) {
   //    multipli di 5 e 3
   if (i % 5 === 0 && i % 3 === 0) {
